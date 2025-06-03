8130dabcfc5b60c8c4ae0feaccf1f0dd566d8ff8
Marcos <123852633+MarcosEJV@users.noreply.github.com>
2025-06-03 11:34:04 -0400

    Update run-analysis.R
    Updated the code so it can work when you are outside the dataset folder as it is instructed on Coursera website.

--- run-analysis.R
+++ run-analysis.R
@@ -2,12 +2,12 @@ library(dplyr)     #Package used for reading and cleaning the data.
 
 #Merges the training and the test sets to create one data set.
 #read all the data for the test and train tables
-xtest <- read.table("./test/X_test.txt")
-ytest <- read.table("./test/Y_test.txt")
-subjecttest <- read.table("./test/subject_test.txt")
-xtrain <- read.table("./train/X_train.txt")
-ytrain <- read.table("./train/Y_train.txt")
-subjecttrain <- read.table("./train/subject_train.txt")
+xtest <- read.table("./UCI HAR Dataset/test/X_test.txt")
+ytest <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+subjecttest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+xtrain <- read.table("./UCI HAR Dataset/train/X_train.txt")
+ytrain <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+subjecttrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
         
 #Merge everything
 xdata <- rbind(xtrain, xtest)     #Binds rows for the x values
@@ -15,12 +15,12 @@ ydata <- rbind(ytrain, ytest)     #Binds rows for the y values
 subjectdata <- rbind(subjecttrain, subjecttest)     #Binds rows for the subjects data
         
 #Extracts only the measurements on the mean and standard deviation for each measurement. 
-features <- read.table("./features.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 mean.std <- features[grep("mean\\(\\)|std\\(\\)",features[,2]),]     #Extracts the mean and std columns names
 xdata <- xdata[,mean.std[,1]]     #Filters the data based on the columns selected for the mean and std
 
 #Uses descriptive activity names to name the activities in the data set
-activitylabels <- read.table("./activity_labels.txt")
+activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 colnames(ydata) <- "Activity"     #Renames the column of the activitylabels table
 ydata$Activity <- factor(ydata$Activity, labels = as.character(activitylabels[,2]))     #Replaces the numbers with the actual names of each activity
         
